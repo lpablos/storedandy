938bb1abb83929de722cc1bbe5b3b67245a913ff
FGE <lpablo@fiscaliaveracruz.gob.mx>
2020-12-23 10:56:54 -0600

    Add library of icons  FontAwesome

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Vuetify from "vuetify";
 import "vuetify/dist/vuetify.min.css";
 import es from "vuetify/lib/locale/es";
+import "@fortawesome/fontawesome-free/css/all.css";
 
 Vue.use(Vuetify);
 
