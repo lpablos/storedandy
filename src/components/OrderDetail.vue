964d092549fe746d7f9fe2bb1f7c40bdb29a142e
FGE <lpablo@fiscaliaveracruz.gob.mx>
2020-12-23 15:29:03 -0600

    Advance on Detial car

---
+++ src/components/OrderDetail.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-btn class="mx-2" fab dark small color="pink">
+    <i class="fas fa-shopping-cart"></i>
+  </v-btn>
+</template>
+<script>
+export default {
+  name: "OrderDetail"
+};
+</script>
