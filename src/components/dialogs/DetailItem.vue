8d6a45bda07569d74dec584e1a295f23bde6c66d
FGE <lpablo@fiscaliaveracruz.gob.mx>
2020-12-23 13:00:34 -0600

    Improvement of detail item

--- src/components/dialogs/DetailItem.vue
+++ src/components/dialogs/DetailItem.vue
@@ -1,46 +1,34 @@
 <template>
   <div class="text-center">
     <v-dialog :persistent="true" v-model="dialog" width="500">
-      <v-card  elevation="24" max-width="500" class="mx-auto">
-        <v-system-bar lights-out></v-system-bar>
-        <v-carousel
-          :continuous="false"
-          :cycle="cycle"
-          :show-arrows="false"
-          hide-delimiter-background
-          delimiter-icon="mdi-minus"
-          height="300"
-        >
-          <v-carousel-item v-for="(slide, i) in slides" :key="i">
-            <v-sheet :color="colors[i]" height="100%" tile>
-              <v-row class="fill-height" align="center" justify="center">
-                <div class="display-3">
-                  {{ slide }} Slide
-                </div>
-              </v-row>
-            </v-sheet>
-          </v-carousel-item>
+      <v-card class="mx-auto" max-width="500">
+        <v-carousel height="300">
+          <v-carousel-item
+            v-for="(item, i) in items"
+            :key="i"
+            :src="item.src"
+            reverse-transition="fade-transition"
+            transition="fade-transition"
+          ></v-carousel-item>
         </v-carousel>
-        <v-list two-line>
-          <v-card-text>
-            <div class="my-4 subtitle-1">
-              $ 15.00 MXN
-            </div>
-
-            <div>
-              Small plates, salads & sandwiches - an intimate setting with.
-              <v-divider class="mx-1"></v-divider>
-            </div>
-            <v-card-actions class="justify-center">
-              <v-btn color="primary" text @click="closeDialog()">
-                Agregar
-              </v-btn>
-              <v-btn color="primary" text @click="closeDialog()">
-                Cerrar
-              </v-btn>
-            </v-card-actions>
-          </v-card-text>
-        </v-list>
+        <v-card-title>
+          Cafe Badilico
+        </v-card-title>
+        <v-card-text>
+          <div class="my-4 subtitle-1">
+            $15.00 MXN
+          </div>
+          <div> Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
+        </v-card-text>
+        <v-divider class="mx-4"></v-divider>
+        <v-card-actions class="justify-center">
+          <v-btn color="primary" text @click="closeDialog()">
+            Agregar
+          </v-btn>
+          <v-btn color="primary" text @click="closeDialog()">
+            Cerrar
+          </v-btn>
+        </v-card-actions>
       </v-card>
     </v-dialog>
   </div>
@@ -49,21 +37,20 @@
 export default {
   data () {
     return {
-      colors: [
-        'green',
-        'secondary',
-        'yellow darken-4',
-        'red lighten-2',
-        'orange darken-1'
-      ],
-      cycle: false,
-      slides: [
-        'First',
-        'Second',
-        'Third',
-        'Fourth',
-        'Fifth'
-      ],
+      items: [
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg"
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg"
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg"
+        }
+      ]
     }
   },
   props: {
