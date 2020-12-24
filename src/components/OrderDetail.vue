923edf1e1a94883d41cf19fbc3cb96536bc50bec
FGE <lpablo@fiscaliaveracruz.gob.mx>
2020-12-24 13:48:51 -0600

    Avance en modal

--- src/components/OrderDetail.vue
+++ src/components/OrderDetail.vue
@@ -1,10 +1,20 @@
 <template>
-  <v-btn class="mx-2" fab dark small color="pink">
+  <v-btn class="mx-2" fab dark small color="pink" v-on:click="dialog = true">
     <i class="fas fa-shopping-cart"></i>
+    <detail-car :show="dialog" />
   </v-btn>
 </template>
 <script>
+import DetailCar from "./dialogs/DetailCar.vue";
 export default {
-  name: "OrderDetail"
+  name: "OrderDetail",
+  data() {
+    return {
+      dialog: false
+    }
+  },
+  components: {
+    DetailCar
+  }
 };
 </script>
--- src/components/dialogs/DetailCar.vue
+++ src/components/dialogs/DetailCar.vue
@@ -1,32 +1,90 @@
 <template>
-  <v-row justify="space-around">
-    <v-col cols="auto">
-      <v-dialog  transition="dialog-bottom-transition" max-width="600">
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn color="primary" v-bind="attrs" v-on="on"> From the bottom </v-btn>
-        </template>
-        <template v-slot:default="dialog">
-          <v-card>
-            <v-toolbar color="primary" dark>Opening from the bottom</v-toolbar>
-            <v-card-text>
-              <div class="text-h2 pa-12">Hello world!</div>
-            </v-card-text>
-            <v-card-actions class="justify-end">
-              <v-btn text @click="dialog.value = false">Close</v-btn>
-            </v-card-actions>
-          </v-card>
-        </template>
-      </v-dialog>
-    </v-col>
-  </v-row>
+  <div class="text-center">
+    <v-dialog v-model="show" width="800">
+      <v-card>
+        <v-card-title class="headline grey lighten-2">
+          Privacy Policy
+        </v-card-title>
+        <v-stepper v-model="e1">
+          <v-stepper-header>
+            <v-stepper-step :complete="e1 > 1" step="1">
+              Mi pedido
+            </v-stepper-step>
+
+            <v-divider></v-divider>
+
+            <v-stepper-step :complete="e1 > 2" step="2">
+              Mis datos
+            </v-stepper-step>
+
+            <v-divider></v-divider>
+
+            <v-stepper-step step="3">
+              Confirmación
+            </v-stepper-step>
+          </v-stepper-header>
+
+          <v-stepper-items>
+            <v-stepper-content step="1">
+              <v-card class="mb-12" color="grey lighten-1" height="200px">
+              </v-card>
+              <v-divider></v-divider>
+              <v-col>
+                <v-btn color="primary" @click="e1 = 2">
+                  Continue
+                </v-btn>
+              </v-col>
+              <v-col class="text-right">
+                <v-btn text>
+                  Cancel
+                </v-btn>
+              </v-col>
+            </v-stepper-content>
+
+            <v-stepper-content step="2">
+              <v-card class="mb-12" color="grey lighten-1" height="200px">
+              </v-card>
+              <v-divider></v-divider>
+              <v-btn color="primary" @click="e1 = 3">
+                Continue
+              </v-btn>
+
+              <v-btn text>
+                Cancel
+              </v-btn>
+            </v-stepper-content>
+
+            <v-stepper-content step="3">
+              <v-card class="mb-12" color="grey lighten-1" height="200px">
+              </v-card>
+              <v-divider></v-divider>
+              <v-btn color="primary" @click="e1 = 1">
+                Continue
+              </v-btn>
+
+              <v-btn text>
+                Cancel
+              </v-btn>
+            </v-stepper-content>
+          </v-stepper-items>
+        </v-stepper>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 <script>
 export default {
   name: "DetailCar",
-  data () {
+  data() {
     return {
-      dialog: false
+      e1: 1
     }
   },
+  props: {
+    show: {
+      default: true,
+      e1: 2
+    }
+  }
 };
 </script>
