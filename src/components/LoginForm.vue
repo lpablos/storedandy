6a1eb6fbc5aaaaf30485dbd9308e77e9fa19f23a
Luis Jorge <gevolutionxalapa@gmail.com>
2021-02-25 14:58:26 -0600

    Login form avance

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-form ref="form" v-model="valid" lazy-validation>
+    <v-card-title>
+      <span class="title font-weight justify-center">Danndy Store</span>
+    </v-card-title>
+    <v-card-text class="headline font-weight-bold">
+      <v-text-field
+        v-model="email"
+        :rules="emailRules"
+        label="E-mail"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="key"
+        type="password"
+        :counter="10"
+        label="Contraseña"
+        required
+      ></v-text-field>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn color="green" dark depressed>
+        Iniciar Sessión
+      </v-btn>
+    </v-card-actions>
+  </v-form>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    valid: true,
+    name: "",
+    key: "",
+    nameRules: [
+      v => !!v || "Name is required",
+      v => (v && v.length <= 10) || "Name must be less than 10 characters",
+    ],
+    email: "",
+    emailRules: [
+      v => !!v || "E-mail is required",
+      v => /.+@.+\..+/.test(v) || "E-mail must be valid",
+    ],
+    select: null,
+    items: ["Item 1", "Item 2", "Item 3", "Item 4"],
+    checkbox: false,
+  }),
+
+  methods: {
+    validate() {
+      this.$refs.form.validate()
+    },
+    reset() {
+      this.$refs.form.reset()
+    },
+    resetValidation() {
+      this.$refs.form.resetValidation()
+    }
+  }
+};
+</script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,13 +1,20 @@
 <template>
-  <div>
-    <main-menu />
-    <div class="login">
-      <h1>Aqui estara el login</h1>
-    </div>
-  </div>
+  <v-container class="grey lighten-5">
+    <v-row no-gutters>
+      <v-col cols="2" sm="4">
+        <v-card class="pa-2 " outlined tile>
+          <login-form />
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 <script>
+import LoginForm from "@/components/LoginForm.vue";
 export default {
-  name: "Login"
+  name: "Login",
+  components: {
+    LoginForm
+  }
 };
 </script>
