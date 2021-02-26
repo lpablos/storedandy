26e45c65bbe26f60978c91895f12aa1cd92b0ea6
Luis Jorge <gevolutionxalapa@gmail.com>
2021-02-26 13:11:18 -0600

    theme admin of administrator

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,55 +1,13 @@
 <template>
   <v-app id="inspire">
-    <v-app-bar app color="white" flat>
-      <v-container class="py-0 fill-height">
-        <v-avatar class="mr-10" color="grey darken-1" size="32"></v-avatar>
-
-        <v-btn v-for="link in links" :key="link" text>
-          {{ link }}
-        </v-btn>
-
-        <v-spacer></v-spacer>
-
-        <v-responsive max-width="260">
-          <v-text-field
-            dense
-            flat
-            hide-details
-            rounded
-            solo-inverted
-          ></v-text-field>
-        </v-responsive>
-      </v-container>
-    </v-app-bar>
-
+    <main-menu-admin />
     <v-main class="grey lighten-3">
       <v-container>
         <v-row>
-          <v-col cols="2">
-            <v-sheet rounded="lg">
-              <v-list color="transparent">
-                <v-list-item v-for="n in 5" :key="n" link>
-                  <v-list-item-content>
-                    <v-list-item-title> List em {{ n }} </v-list-item-title>
-                  </v-list-item-content>
-                </v-list-item>
-
-                <v-divider class="my-2"></v-divider>
-
-                <v-list-item link color="grey lighten-4">
-                  <v-list-item-content>
-                    <v-list-item-title>
-                      Refresh
-                    </v-list-item-title>
-                  </v-list-item-content>
-                </v-list-item>
-              </v-list>
-            </v-sheet>
-          </v-col>
-
+          <vertical-menu-admin />
           <v-col>
             <v-sheet min-height="70vh" rounded="lg">
-              <!--  -->
+              <h4>Todo contenido aqui</h4>
             </v-sheet>
           </v-col>
         </v-row>
@@ -59,9 +17,13 @@
 </template>
 
 <script>
+import MainMenuAdmin from "@/components/menus/MainMenuAdmin.vue";
+import VerticalMenuAdmin from "@/components/menus/VerticalMenuAdmin.vue";
 export default {
-  data: () => ({
-    links: ["Mi cuenta", "Productos", "Configuración"]
-  })
+  name: "Admin",
+  components: {
+    MainMenuAdmin,
+    VerticalMenuAdmin
+  }
 };
 </script>
