0325328ec2e0b65cf96161476383883ba4db1309
Luis Jorge <gevolutionxalapa@gmail.com>
2021-02-26 10:47:54 -0600

    Theme for admin section

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Login from "../views/Login.vue";
+import Admin from "../views/Admin.vue";
 
 Vue.use(VueRouter);
 
@@ -15,16 +16,12 @@ const routes = [
     path: "/admin/login",
     name: "Login",
     component: Login
+  },
+  {
+    path: "/admin/index",
+    name: "admin",
+    component: Admin
   }
-  // {
-  //   path: "/about",
-  //   name: "About",
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () =>
-  //     import(/* webpackChunkName: "about" */ "../views/About.vue")
-  // }
 ];
 
 const router = new VueRouter({
